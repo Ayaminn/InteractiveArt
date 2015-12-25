623becb1a5c6c3d3ffc6dcbe61ee9c3fda02cb72
Ayami_Imai <ayami0727tu@yahoo.co.jp>
2015-12-25 13:26:51 +0900

    メイン画面でサブ画面のゲームオブジェクトが見えないようにした

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,13 +13,14 @@ public class Ball : MonoBehaviour {
 	void Update () {
 		timeleft -= Time.deltaTime;
 
-		if (timeleft <=  0.0f) {
+		if (timeleft <= 0.0f) {
 			timeleft = 1.0f;
+
+			if (this.transform.position.y <= 80){
+				Destroy (gameObject);
+			}
+
 			transform.position += new Vector3 (0, -0.3f * Time.deltaTime, 0);
-		}
-
-		if (gameObject.transform.position.y <= 50){
-
 		}
 	}
 }
