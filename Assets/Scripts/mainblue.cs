695aa283082dc8cb0f9b97042c8f462e6d5b6644
Ayami_Imai <ayami0727tu@yahoo.co.jp>
2015-12-28 22:50:33 +0900

    タップ成功数によって光る雪だるまが変わるようにコード追加

--- Assets/Scripts/mainblue.cs
+++ Assets/Scripts/mainblue.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class mainblue : Photon.MonoBehaviour {
 	
-	public ParticleSystem par;
-	public PhotonView myPV ;
+	public ParticleSystem parred;
+	public ParticleSystem paryellow;
+	public ParticleSystem parblue;
+	public ParticleSystem pargreen;
+	public PhotonView myPV;
 	public GameObject BlueHantei;
 	public GameObject Onpu;
 	public Camera cameran;
 	bool HanabiFlag = false;
+	public int score = 0;
+	public GameObject pink;
+	public GameObject blue;
+	public GameObject yellow;
+	public float ram;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,7 +32,8 @@ public class mainblue : Photon.MonoBehaviour {
             	GameObject obj = hit.collider.gameObject;
 
            		if(obj == BlueHantei || obj == Onpu){
-           			Debug.Log("ll");
+					score++;
+           			Debug.Log("Good!");
 					Destroy(other.gameObject);
 					myPV.RPC("hanabi",PhotonTargets.All);
            		}
@@ -34,12 +43,36 @@ public class mainblue : Photon.MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(score == 5){
+			blue.SetActive (false);
+			pink.SetActive (false);
+			yellow.SetActive (true);
+		}else if(score == 15){
+			blue.SetActive (true);
+			pink.SetActive (false);
+			yellow.SetActive (false);
+		}else if(score == 30){
+			blue.SetActive (false);
+			pink.SetActive (true);
+			yellow.SetActive (false);
+		}else if(score == 45){
+			blue.SetActive (true);
+			pink.SetActive (false);
+			yellow.SetActive (true);
+		}else if(score == 60){
+			blue.SetActive (true);
+			pink.SetActive (true);
+			yellow.SetActive (true);
+		}
 	}
 
 	[RPC]
 
 	void hanabi(){
-		par.Play();
+		ram = Random.Range (7, 40);
+		if(score % 3 == 0){
+			paryellow.transform.position = new Vector3 (ram, 10f, -20f);
+			paryellow.Play();
+		}
 	}
 }
