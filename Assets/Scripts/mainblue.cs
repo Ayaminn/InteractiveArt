f983d64c196b0e0ecdf76e61c2c2c1e5a34566e8
Ayami_Imai <ayami0727tu@yahoo.co.jp>
2015-12-29 02:26:33 +0900

    androidとの通信完璧に！

--- Assets/Scripts/mainblue.cs
+++ Assets/Scripts/mainblue.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class mainblue : Photon.MonoBehaviour {
-	
+
 	public ParticleSystem parred;
 	public ParticleSystem paryellow;
 	public ParticleSystem parblue;
@@ -17,30 +17,31 @@ public class mainblue : Photon.MonoBehaviour {
 	public GameObject blue;
 	public GameObject yellow;
 	public float ram;
-	
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	void OnTriggerStay(Collider other){
-		
+
 		if (Input.GetMouseButtonDown(0)) {
-       		Ray ray = cameran.ScreenPointToRay(Input.mousePosition);
-       		RaycastHit hit = new RaycastHit();
+			Ray ray = cameran.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit = new RaycastHit();
 
-        	if (Physics.Raycast(ray, out hit)){
-            	GameObject obj = hit.collider.gameObject;
+			if (Physics.Raycast(ray, out hit)){
+				GameObject obj = hit.collider.gameObject;
 
-           		if(obj == BlueHantei || obj == Onpu){
-					score++;
-           			Debug.Log("Good!");
+				if(obj == BlueHantei || obj == Onpu){
+					//score++;
+					Debug.Log("Good!");
 					Destroy(other.gameObject);
 					myPV.RPC("hanabi",PhotonTargets.All);
-           		}
-       		}
+					myPV.RPC("plus",PhotonTargets.All);
+				}
+			}
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(score == 5){
@@ -68,6 +69,11 @@ public class mainblue : Photon.MonoBehaviour {
 
 	[RPC]
 
+	void plus(){
+		score++;
+	}
+
+	[RPC]
 	void hanabi(){
 		ram = Random.Range (7, 40);
 		if(score % 3 == 0){
