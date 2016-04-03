94ada774e6ea0886d2a2386c86ccc59c92d6e6b5
Ayaminn <ayamin.imai@gmail.com>
2016-04-03 10:33:13 +0900

    シンデレラフェス用に

---
+++ Assets/Scripts/FireFlower.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireFlower : MonoBehaviour {
+
+	private float timeleft;
+	private float ramx;
+	private float ramh;
+	public GameObject pink;
+	public GameObject blue;
+	public GameObject green;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeleft -= Time.deltaTime;
+		ramh = Random.Range (1, 3);
+		if (timeleft <= 0.0) {
+			ramx = Random.Range (-6, 6);
+			blue.SetActive (false);
+			pink.SetActive (false);
+			green.SetActive (false);
+			timeleft = 5.0f;
+			if (ramh == 1) {
+				blue.SetActive (false);
+				pink.SetActive (false);
+				green.SetActive (true);
+				green.transform.position = new Vector3 (ramx, 0, 0);
+			} else if (ramh == 2) {
+				blue.SetActive (false);
+				pink.SetActive (true);
+				green.SetActive (false);
+				pink.transform.position = new Vector3 (ramx, 0, 0);
+			} else if (ramh == 3) {
+				blue.SetActive (true);
+				pink.SetActive (false);
+				green.SetActive (true);
+				blue.transform.position = new Vector3 (ramx, 0, 0);
+			}
+		}
+	}
+}
